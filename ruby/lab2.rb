1bfff44b8017968d40a0f9403e495531db9e80d9
ibadboy <polawat.job@gmail.com>
2016-04-25 03:30:30 +0000

    add homework

---
+++ ruby/lab2.rb
@@ -0,0 +1,9 @@
+puts "สวัสดีจ้าฉันชื่อ บุษบา"
+print "คุณชื่ออะไร หึ?"
+name = gets.chomp
+puts "สวัสดีจ้า #{name}"
+print "คุณ #{name} อายุเท่าไร หึ"
+ages = gets.chomp
+puts "ทำไมคุณ #{name} แก่จัง"
+
+
